d4fe7cb93734c80eec982e76fd259e0d993863ac
Toh-Ma <92999833+Toh-Ma@users.noreply.github.com>
2022-01-06 14:40:46 +0100

    Update1

--- connect_sql.php
+++ connect_sql.php
@@ -1,8 +1,8 @@
 <?php
-$servername="e-srv-lamp";
-$username_db="e182457";
-$password_db="end61ch";
-$database="e182457";
+$servername="";
+$username_db="";
+$password_db="";
+$database="";
 
 $conn = mysqli_connect($servername, $username_db, $password_db, $database);
 ?>
