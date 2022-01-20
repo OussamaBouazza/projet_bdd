fc2d9912ca9702a36e5177c4db823a51068a818c
Toh-Ma <92999833+Toh-Ma@users.noreply.github.com>
2022-01-20 17:28:13 +0100

    Style

--- co_supprimer.php
+++ co_supprimer.php
@@ -19,7 +19,7 @@
         <!-- Barre de navigation -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-        <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #5e5f5f;">
+        <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:black;">
             <a class="navbar-brand" href="acceuil.html">Acceuil</a>
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
