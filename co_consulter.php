02030075900166651ebbec707fa13459f6d85578
OussamaBouazza <oussama-bouazza@outlook.com>
2022-01-20 22:53:34 +0100

    consulter une commande

--- co_consulter.php
+++ co_consulter.php
@@ -40,15 +40,78 @@
             
             else if(isset($_POST['Consulter'])){
                 $id_commande=$_POST["id_commande"];
-                echo "<p style='color:green'>Commande :</p>";
+                    echo "<p style='color:green'>Commande :</p>";
 
-                include "connect_sql.php";
-                $query="SELECT ...";
-                $result = $conn->query(utf8_decode($query));       
+                    include "connect_sql.php";
+                    
+                    //requête pour afficher le clien, la date et le montant de la commande
+                    $query = "SELECT nom_client, prix, date FROM commande 
+                    NATURAL JOIN client
+                    WHERE id_order = $id_commande;";
+
+                    $result = $conn->query(utf8_decode($query));
+
+                    //vérifier si le numéro de commande saisi est dans la bdd
+                    if($result->num_rows == 0){
+                        echo "<h2>Erreur : le numéro de commande saisi est incorrect</h2>";
+                    }
+                    else
+                    {
+                        $data = mysqli_fetch_array($result);
+                        
+                        $nom_client = $data["nom_client"];
+                        $prix_commande = $data["prix"];
+                        $date_commande = $data["date"];
+    
+                        echo "
+                            <ul>
+                                <li>Client : $nom_client</li>
+                                <li>Date de la commande : ". date("d/m/Y", strtotime($date_commande)) ."</li>
+                                <li>Prix total de la commande : $prix_commande €</li>
+                            </ul>";
+    
+                        //construction du tableau contenant les détails de la commande
+                        $tableau = "<table>
+                                        <thead>
+                                            <th>Article</th>
+                                            <th>Prix unitaire</th>
+                                            <th>Quantité</th>
+                                            <th>Prix total</th>
+                                            <th>Date d'expédition</th>
+                                            <th>Date de livraison</th>
+                                        </thead>
+                                        <tbody>
+                        ";
+    
+                        $query = "SELECT nom_item, item_price, quantite, prix_total, date_expedition, date_livraison FROM order_content
+                            NATURAL JOIN item
+                            WHERE id_order=$id_commande;";
+    
+                        $result = $conn->query(utf8_decode($query));
+                        
+                        while($row = mysqli_fetch_assoc($result)){
+                            $tableau .= "
+                                <tr>
+                                    <th>$row[nom_item]</th>
+                                    <th>$row[item_price]</th>
+                                    <th>$row[quantite]</th>
+                                    <th>$row[prix_total]</th>
+                                    <th>". date("d/m/Y", strtotime($row["date_expedition"])) ."</th>
+                                    <th>". date("d/m/Y", strtotime($row["date_livraison"])) ."</th>
+                                </tr>
+                            ";
+                        }
+    
+                        $tableau .= "</tbody></table>";
+                        echo $tableau;               
+
+                    }
                 }
-            
+
             ?>
         </div> 
+        
+        
             
         </main>                       
 		    <!--FIN PARTIE CENTRALE-->
