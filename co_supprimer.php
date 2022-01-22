2999648bb4215a0958d574ed52911ed6d422439e
OussamaBouazza <oussama-bouazza@outlook.com>
2022-01-22 18:33:18 +0100

    suppression de commande

--- co_supprimer.php
+++ co_supprimer.php
@@ -1,61 +1,134 @@
 <!DOCTYPE html>
 
-<html lang="fr"> 					<!-- Page française -->
-    <head>					<!-- Nom qui apparait sur l'onglet de navigation -->
-        <title> Supprimer Commande </title>
+<html lang="fr">
+<!-- Page française -->
 
-        <meta charset="utf-8">
+<head>
+    <!-- Nom qui apparait sur l'onglet de navigation -->
+    <title> Supprimer Commande </title>
 
-		<link rel="stylesheet" href="css/style1.css" />		<!-- lien entre html et CSS -->
+    <meta charset="utf-8">
 
-    </head>
-    <body>
-        <header>					<!-- Barre d'informations -->
+    <link rel="stylesheet" href="css/style1.css" /> <!-- lien entre html et CSS -->
 
-        </header>
+</head>
 
-        <main>            
+<body>
+    <header>
+        <!-- Barre d'informations -->
+        <!-- Barre de navigation -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-                <!--PARTIE CENTRALE -->
+        <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:black;">
+            <a class="navbar-brand" href="acceuil.html">Acceuil</a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="co_consulter">Consulter</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="co_creer">Creer</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="co_modifier">Modifier</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="co_supprimer"><span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="co_liste">Liste</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="co_facture">Facture</a>
+                    </li>
+
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+
+        <!--PARTIE CENTRALE -->
 
         <div id="container1">
             <!-- zone de formulaire -->
 
             <form action="co_supprimer.php" method="POST">
                 <h1>Supprimer une commande</h1>
-				
-				<label for="id_client"><b>Id:</b></label>
-                <input type="text" placeholder="Entrer ID de la commande à supprimer:" name="id_order" required>
-				
-	
+
+                <label for="id_client"><b>Id:</b></label>
+                <input type="number" min="1" placeholder="Entrer ID de la commande à supprimer:" name="id_order" required>
+
+
                 <input type="submit" id='ButtonSupp' value='Supprimer' name="Supprimer">
             </form>
 
-                <?php
+            <?php
 
-                if(isset($_GET['erreur'])){
-                    $err = $_GET['erreur'];
-                    if($err==1 || $err==2) 
-                        echo "<p style='color:red'>Données incohérentes</p>";
-				}
-				
-                else if(isset($_POST['Supprimer'])){
-                    $id_order=$_POST["id_order"];
-                    echo "<p style='color:green'>Supprimé</p>";
-                    
-                    include "connect_sql.php";
-		            $query="DELETE FROM Order WHERE id_order='$id_order'";
+            include "connect_sql.php";
+
+            if (isset($_GET['erreur'])) {
+                $err = $_GET['erreur'];
+                if ($err == 1 || $err == 2)
+                    echo "<p style='color:red'>Données incohérentes</p>";
+
+            } else if (isset($_POST['Supprimer'])) {
+                $id_order = $_POST["id_order"];
+
+                $query = "SELECT * FROM commande WHERE id_order=$id_order";
+                $result = $conn->query(utf8_decode($query));
+                $row = mysqli_fetch_array($result);
+
+                if ($row == NULL) {
+                    echo "
+                        <div class='client-details'>
+                            Cet ID ne correspond à aucune commande.
+                        </div>";
+                } else {
+                    $item_list = [];
+
+                    $query = "SELECT id_item, quantite FROM order_content WHERE id_order=$id_order;";
                     $result = $conn->query(utf8_decode($query));
-                }
-                
-				?>
-                 <p id="idBarreInfo"> Entrez l'id puis cliquez sur Supprimer. </p>
-        </div> 
-            
-        </main>                       
-		    <!--FIN PARTIE CENTRALE-->
-        <!-- Pied de page -->
-        
-    </body>
-</html>
 
+                    //mettre dans $item_list les id_item et la quantité à ssupprimer
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $obj = new stdClass;
+                        $obj->id_item = $row["id_item"];
+                        $obj->quantite = $row["quantite"];
+
+                        array_push($item_list, $obj);
+                    }
+
+                    $query = "DELETE FROM order_content WHERE id_order=$id_order";
+                    $result = $conn->query(utf8_decode($query));
+                    
+                    $query = "DELETE FROM commande WHERE id_order=$id_order;";
+                    $result = $conn->query(utf8_decode($query));
+
+                    //mettre à jour le stock
+                    foreach ($item_list as $item) {
+                        $query = "UPDATE `item` SET `stock` = stock + $item->quantite WHERE id_item= $item->id_item;";
+                        $result = $conn->query(utf8_decode($query)); 
+                    }
+                    
+                    echo "<p style='color:red'>Supprimé</p>";
+                }
+            }
+
+            ?>
+            <p id="idBarreInfo"> Entrez l'id puis cliquez sur Supprimer. </p>
+        </div>
+
+    </main>
+    <!--FIN PARTIE CENTRALE-->
+    <!-- Pied de page -->
+
+</body>
+
+</html>
